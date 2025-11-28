9577ade75de0bfcaddfd5a371b9f9779e7ac3e51
JARP <105678913+anderson895@users.noreply.github.com>
2025-11-28 17:47:56 +0800

    update

--- vlogs/views.py
+++ vlogs/views.py
@@ -1,4 +1,3 @@
-# vlogs/views.py
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
